693cf4a4bb35e6b38cb346684a1fd2fba6ed451f
Warren <warren.s.488@gmail.com>
2022-07-18 14:07:21 -0400

    display list of events and add click eventfor displaying markets; add fetch market logic

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,39 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    liveEvents: [],
+    liveMarkets: [],
+    loadedEvents: false,
+  },
   getters: {},
   mutations: {},
-  actions: {},
+  actions: {
+    async getLiveEvents(context) {
+      if (!context.state.loadedEvents) {
+        return fetch("http://localhost:8888/football/live?primaryMarkets=true")
+          .then(async (res) => {
+            const { events } = await res.json();
+            context.state.liveEvents = events;
+            context.state.loadedEvents = true;
+          })
+          .catch(console.log);
+      }
+    },
+    async loadMarket(context, marketId) {
+      const existingMarket = context.state.liveMarkets.find(
+        ({ marketId: id }) => marketId === id
+      );
+      if (!existingMarket) {
+        return fetch("http://localhost:8888/sportsbook/market/" + marketId)
+          .then(async (res) => {
+            const { market } = await res.json();
+            // @ts-ignore
+            context.state.liveMarkets = [...context.state.liveMarkets, market];
+          })
+          .catch(console.log);
+      }
+    },
+  },
   modules: {},
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Live Events</h1>
+    <ul>
+      <li v-for="event in liveEvents" :key="event.eventId">
+        {{ event.name }}
+        <p @click="() => setMarketsVisible(event.eventId)">see more...</p>
+        <div v-if="visibleMarkets.includes(event.eventId)">
+          {{ event.markets }}
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { mapState } from "vuex";
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      visibleMarkets: [""],
+    };
+  },
+  methods: {
+    setMarketsVisible(id: string) {
+      console.log(id);
+      if (this.visibleMarkets.includes(id)) {
+        this.visibleMarkets = this.visibleMarkets.filter(
+          (arrId: string) => arrId !== id
+        );
+      } else {
+        this.visibleMarkets.push(id);
+      }
+    },
+  },
+  computed: {
+    ...mapState(["liveEvents"]),
   },
 });
 </script>
+
+<style lang="scss" scoped>
+p {
+  margin: 0px;
+}
+</style>
