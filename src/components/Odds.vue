e78530f28d529d6b2db9268a753f008144a58ec9
Warren <warren.s.488@gmail.com>
2022-07-19 06:25:54 -0400

    delete hello world and add odds component

---
+++ src/components/Odds.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="odds">
+    <div>{{ decimalView ? decimal : `${num}/${den}` }}</div>
+    <button @click="decimalView = !decimalView">
+      {{ decimalView ? "fract" : "decimal" }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Odd-Component",
+  props: {
+    den: Number,
+    num: Number,
+    decimal: Number,
+  },
+  data() {
+    return {
+      decimalView: false,
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.odds {
+  display: flex;
+}
+</style>
