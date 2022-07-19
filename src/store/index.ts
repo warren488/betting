cfda2779443f41ee61da8089d3574e86077b3564
Warren <warren.s.488@gmail.com>
2022-07-19 11:53:19 -0400

    add option to subscribe to market changes; add some documentation; only try to update market if it exists

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,6 +13,7 @@ export default createStore({
   getters: {},
   mutations: {},
   actions: {
+    /** loads an event into its own object for the event page, also makes it easier to subscribe to changes */
     async loadCurrentEvent(context, eventId) {
       return fetch("http://localhost:8888/sportsbook/event/" + eventId).then(
         async (res) => {
@@ -22,6 +23,7 @@ export default createStore({
         }
       );
     },
+    /** get all live events to display on the homoe page */
     async getLiveEvents(context) {
       if (!context.state.loadedEvents) {
         return fetch("http://localhost:8888/football/live?primaryMarkets=true")
@@ -29,6 +31,7 @@ export default createStore({
             const { events } = await res.json();
             context.state.liveEvents = events;
             context.state.loadedEvents = true;
+            // once we get the events we want to add handlers for changes (not actually subscribing yet)
             context.dispatch("listenToOutcomeUpdates");
             context.dispatch("listenToMarketUpdates");
           })
@@ -39,17 +42,21 @@ export default createStore({
       context.state.socket.addEventListener("message", (m) => {
         const message = JSON.parse(m.data);
         console.log(message);
+        // from what I've seen market status is the only event for markets
         if (message.type === "MARKET_STATUS") {
-          console.log("new market");
+          // figure out which market we want to update
           const { index, object: oldMarket } = getObjectBy(
             { marketId: message.data.marketId },
             context.state.liveMarkets
           );
-          context.state.liveMarkets.splice(index, 1, {
-            ...oldMarket,
-            // @ts-ignore
-            status: message.data.status,
-          });
+          // replace the old data with the updated status only if the market exists
+          if (oldMarket) {
+            context.state.liveMarkets.splice(index, 1, {
+              ...oldMarket,
+              // @ts-ignore
+              status: message.data.status,
+            });
+          }
         }
       });
     },
@@ -57,12 +64,15 @@ export default createStore({
       context.state.socket.addEventListener("message", (m) => {
         const message = JSON.parse(m.data);
         console.log(message);
+        // these two events seem to be the only outcome events and they change the same properties
         if (["OUTCOME_STATUS", "PRICE_CHANGE"].includes(message.type)) {
+          // figure out which outcome we want to update
           const { index, object: oldOutcome } = getObjectBy(
             { outcomeId: message.data.outcomeId },
             context.state.outcomes
           );
           console.log(index, oldOutcome);
+          // replace the old data with the updated status only if the outcome exists
           if (oldOutcome) {
             context.state.outcomes.splice(index, 1, {
               ...oldOutcome,
@@ -75,9 +85,10 @@ export default createStore({
         }
       });
     },
-    async loadMarket(context, { marketId, loadOutcomes }) {
+    async loadMarket(context, { marketId, loadOutcomes, subscribe }) {
       console.log("loadMarket");
 
+      // this will stop us from constantly requesting data that already exists
       const existingMarket = context.state.liveMarkets.find(
         ({ marketId: id }) => marketId === id
       );
@@ -87,14 +98,18 @@ export default createStore({
             const { market } = await res.json();
             // @ts-ignore
             context.state.liveMarkets = [...context.state.liveMarkets, market];
-            // as soon as we get the new market and add it to state we listen for any changes
-            context.state.socket.send(
-              JSON.stringify({
-                type: "subscribe",
-                keys: ["m." + marketId],
-                clearSubscription: false,
-              })
-            );
+            // as soon as we get the new market and add it to state we may want to listen for any changes
+            // this subscribe argument helps achieve that
+            if (subscribe) {
+              context.state.socket.send(
+                JSON.stringify({
+                  type: "subscribe",
+                  keys: ["m." + marketId],
+                  clearSubscription: false,
+                })
+              );
+            }
+            // similar to above we may want to immediately load the outcomes
             if (loadOutcomes) {
               context.dispatch("loadOutcomes", market.outcomes);
             }
@@ -111,6 +126,7 @@ export default createStore({
     async loadOutcome(context, outcomeId) {
       console.log("loadoutcome");
 
+      // helps prevent unnecessary requests
       const existingoutcome = context.state.outcomes.find(
         ({ outcomeId: id }) => outcomeId === id
       );
